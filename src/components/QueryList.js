ada0f41ac49edc61f0b2411506cee7bf5276e0ce
Ishank Gupta <ishank.g@taboola.com>
2020-10-05 05:19:43 +0530

    <Interim Commit> Material UI table implemented

--- src/components/QueryList.js
+++ src/components/QueryList.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
 import QueryItem from './QueryItem';
 import PropTypes from 'prop-types';
+import DataTable from 'react-data-table-component';
+import MaterialTable from 'material-table'
 
 class QueryList extends Component {
     
     render() {
         // console.log(this.props.querylist);
-        return this.props.querylist.slice(0).reverse().map((query, index) => (  //.slice(0).reverse() to reverse the array
-            <div>
-                <QueryItem key={query.id} query={query} index={index+1} userHash={this.props.userHash} toggleImportant={this.props.toggleImportant} toggleRead={this.props.toggleRead}/>
-            </div>
-        ));
+        const data = this.props.querylist.slice(0).reverse();
+        // console.log(this.props.querylist);
+        // console.log("this is data");
+        // console.log(data);
+        const columns = [
+        {
+            title: 'location',
+            field: 'location'
+            
+        },
+        {
+            title: 'queryBody',
+            field: 'queryBody'
+        },
+        {
+            title: 'querySubject',
+            field: 'querySubject'
+        }
+        ];
+
+        return (
+        <MaterialTable
+            data={data}
+            columns={columns}
+            options={{
+                filtering:true,
+                exportButton: true
+            }}
+        />
+        );
     }
 }
 
