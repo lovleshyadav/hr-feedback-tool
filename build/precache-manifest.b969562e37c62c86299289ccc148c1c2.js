7fa3f7f82e4332e9a0a3b409392ed8ac66d5703a
Lovlesh Yadav <lovlesh.y@taboola.com>
2021-06-11 22:52:55 +0530

    adding new build

---
+++ build/precache-manifest.b969562e37c62c86299289ccc148c1c2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100de535ecbaebae371c1b4554e3aca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b322bb2f9493129a3aff",
+    "url": "/static/css/main.6d480369.chunk.css"
+  },
+  {
+    "revision": "1491bb4d1e0a2442e4b6",
+    "url": "/static/js/2.87dab6bb.chunk.js"
+  },
+  {
+    "revision": "08f53cb25f158330d2f8b33d96e0349f",
+    "url": "/static/js/2.87dab6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79aaa1e2217b846025c1",
+    "url": "/static/js/3.44aa64d9.chunk.js"
+  },
+  {
+    "revision": "228f656a20d54cfdf7ae",
+    "url": "/static/js/4.a36a0943.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.a36a0943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2314fc85a7561b869bbf",
+    "url": "/static/js/5.09691578.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.09691578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b322bb2f9493129a3aff",
+    "url": "/static/js/main.7e1a711f.chunk.js"
+  },
+  {
+    "revision": "f470852b085a7d2a2ef0",
+    "url": "/static/js/runtime-main.1257b4d2.js"
+  },
+  {
+    "revision": "baf0a253922003aa6e372185fc948212",
+    "url": "/static/media/login_img.baf0a253.jpg"
+  },
+  {
+    "revision": "74683aa8420dd5c1ec98124aaf5c9592",
+    "url": "/static/media/shoutout_bg.74683aa8.jpg"
+  }
+]);
