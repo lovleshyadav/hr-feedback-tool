911d32ef790eecb44898cb74f7226f984a95bf60
Lovlesh Yadav <lovlesh.y@taboola.com>
2021-07-21 09:04:01 +0530

    Updating build files

---
+++ build/precache-manifest.3d6c7522c4a4f8ccefbcb827f9f13d7b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be8a673c56da046a327bb74f72f19e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b9d4a2d1cc806e9f0df",
+    "url": "/static/css/main.6d480369.chunk.css"
+  },
+  {
+    "revision": "f148cb5b357cbd5288ed",
+    "url": "/static/js/2.a7863e3d.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/2.a7863e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3ca7bade3b94aa81c2",
+    "url": "/static/js/3.90ed6a37.chunk.js"
+  },
+  {
+    "revision": "8d052a5fd54999f59e8797ba5bc2d192",
+    "url": "/static/js/3.90ed6a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7483216331e7cec5c7ba",
+    "url": "/static/js/4.c7ff8287.chunk.js"
+  },
+  {
+    "revision": "809abad1ab3333af372fdc0cd7f2829d",
+    "url": "/static/js/4.c7ff8287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "182ebac185884fc64f72",
+    "url": "/static/js/5.88116565.chunk.js"
+  },
+  {
+    "revision": "e4dc15c51dcdfde26486b146e1acda4f",
+    "url": "/static/js/5.88116565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9d4a2d1cc806e9f0df",
+    "url": "/static/js/main.ef44ad6f.chunk.js"
+  },
+  {
+    "revision": "62e485c0c5e04b598395",
+    "url": "/static/js/runtime-main.4eca5532.js"
+  },
+  {
+    "revision": "baf0a253922003aa6e372185fc948212",
+    "url": "/static/media/login_img.baf0a253.jpg"
+  },
+  {
+    "revision": "74683aa8420dd5c1ec98124aaf5c9592",
+    "url": "/static/media/shoutout_bg.74683aa8.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b969562e37c62c86299289ccc148c1c2.js"
+  "/precache-manifest.3d6c7522c4a4f8ccefbcb827f9f13d7b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
